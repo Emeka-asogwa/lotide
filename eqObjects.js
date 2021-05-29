020fcae9fc667685e409276b1c45e3e419307fe7
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-05-29 19:11:07 +0000

    Added more files

---
+++ eqObjects.js
@@ -0,0 +1,56 @@
+
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (let value = 0; value < actual.length; value++) {
+    if (actual[value] !== expected[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (((Object.keys(object1)).length) !== ((Object.keys(object2)).length)) {
+    return false;
+  }
+  for (let values of (Object.keys(object1))) {
+    if (Array.isArray(object1[values]) && Array.isArray(object2[values])) {
+      if (!eqArrays(object1[values], object2[values]))  {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑🤓 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
