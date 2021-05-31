e43d4051265a831ef206434cb72fbe8438b2bfca
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-05-31 23:56:36 +0000

    Updated the refactoring of 'map'

---
+++ test/mapTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+
+const map = require('../map');
+
+
+describe('#map', () => {
+  it('return [ "g", "c", "t", "m", "t" ]', () => {
+    assert.deepEqual(map(words, word => word[0]), [ 'g', 'c', 't', 'm', 't' ])
+  });
+})
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
