63e84576706ddd90960bdfa0817062f8344668a7
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-05-31 19:05:39 +0000

    refactored middle.js

--- middle.js
+++ middle.js
@@ -1,35 +1,3 @@
-
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let value = 0; value < actual.length; value++) {
-    if (actual[value] !== expected[value]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑🤓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/*
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑🤓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-*/
-
-
 const middle = function(arry) {
   let middleArry = [];
   if (arry.length <= 2) {
@@ -45,17 +13,7 @@ const middle = function(arry) {
   return middleArry;
 };
 
+module.exports = middle;
 
 
-console.log(middle([1, 2,6,4, 3,7, 3]));
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
-
-
-
-assertArraysEqual(middle([1, 2,6,4, 3,7, 3]),[4]);
 
