ada9a79715a1cb5d09dfbc5d529ec7ba81e1a8f2
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-23 01:06:34 +0000

    ✍🏽 refactor code and add explicit comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,15 @@
 const eqArrays = function(actual, expected) {
+// ---- check first if they have the same lengths -----//
   if (actual.length !== expected.length) {
     return false;
   }
   for (let value = 0; value < actual.length; value++) {
+    // ---- check second if they have the same values at the same index ---//
     if (actual[value] !== expected[value]) {
       return false;
     }
   }
+  // ----- return true if they have the same values and length ---//
   return true;
 };
 
